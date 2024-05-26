408f313f0c4da008cfc327becf9dac49f1fede12
andreyB <izenkool@gmail.com>
2024-05-26 15:32:06 +0300

    web test

--- tests/Web/travelCostTest.php
+++ tests/Web/travelCostTest.php
@@ -12,17 +12,13 @@ class travelCostTest extends WebTestCase
     #[CoversNothing]
     public function testTravelCostEndpoint(): void
     {
-        // This calls KernelTestCase::bootKernel(), and creates a
-        // "client" that is acting as the browser
         $client = static::createClient();
 
-        // Request a specific page
-        $crawler = $client->request('GET', '/api/v1/travel/cost?cost=100&birthDate=2020-10-18');
+        $client->request('GET', '/api/v1/travel/cost?cost=100&birthDate=2020-10-18');
 
-        // Validate a successful response and some content
         $this->assertResponseIsSuccessful();
-//        $this->assertSelectorTextContains('h1', 'Hello World');
-
-//        $this->assertGreaterThan(0, $crawler->filter('html:contains("Hello World")')->count());
+        $response = $client->getResponse();
+        $this->assertTrue($response->headers->contains('Content-Type', 'application/json'));
+        $this->assertJson($response->getContent());
     }
 }
