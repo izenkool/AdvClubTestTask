667ec781f0ed9773e77bc94cce75a77dd20b3546
andreyB <izenkool@gmail.com>
2024-05-26 15:05:20 +0300

    response doc

--- src/Controller/TravelController.php
+++ src/Controller/TravelController.php
@@ -5,11 +5,15 @@ namespace App\Controller;
 use App\Request\TravelCostDTO;
 use App\Service\Discount\EarlyBookingDiscountService;
 use App\Service\Discount\KidDiscountService;
+use DateTimeImmutable;
+use Exception;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 use Symfony\Component\Routing\Attribute\Route;
+use OpenApi\Attributes as OA;
 
 #[AsController]
 #[Route('/api/v1/travel')]
@@ -21,22 +25,38 @@ class TravelController
     }
 
     #[Route('/cost', name: 'app_travel_cost', methods: ['GET'])]
+    #[OA\Response(
+        response: 200,
+        description: 'Returns cost with discounts',
+        content: new OA\JsonContent(
+            properties: [new OA\Property(property: 'cost', type: 'int')],
+            type: 'object'
+        )
+    )]
+    #[OA\Response(
+        response: 422,
+        description: 'Incorrect request',
+        content: new OA\JsonContent(
+            properties: [new OA\Property(property: 'error', type: 'string')],
+            type: 'object'
+        )
+    )]
     public function cost(
         #[MapQueryString] TravelCostDTO $travelCostDTO,
         KidDiscountService $kidDiscountService,
         EarlyBookingDiscountService $earlyBookingDiscountService,
     ): JsonResponse {
         try {
-            $birthDate = new \DateTimeImmutable($travelCostDTO->getBirthDate());
+            $birthDate = new DateTimeImmutable($travelCostDTO->getBirthDate());
             $cost = $travelCostDTO->getCost() - $kidDiscountService->getDiscount($travelCostDTO->getCost(), $birthDate);
 
-            $startDate = $travelCostDTO->getStartDate() ? new \DateTimeImmutable($travelCostDTO->getStartDate()) : null;
-            $payDate = $travelCostDTO->getPayDate() ? new \DateTimeImmutable($travelCostDTO->getPayDate()) : null;
+            $startDate = $travelCostDTO->getStartDate() ? new DateTimeImmutable($travelCostDTO->getStartDate()) : null;
+            $payDate = $travelCostDTO->getPayDate() ? new DateTimeImmutable($travelCostDTO->getPayDate()) : null;
             $cost = $cost - $earlyBookingDiscountService->getDiscount($cost, $startDate, $payDate);
-        } catch (\Exception $exception) {
+        } catch (Exception $exception) {
             $this->logger->error($exception->getMessage());
 
-            return new JsonResponse(['error' => $exception->getMessage()]);
+            return new JsonResponse(['error' => $exception->getMessage()], Response::HTTP_UNPROCESSABLE_ENTITY);
         }
 
         return new JsonResponse(['cost' => $cost]);
